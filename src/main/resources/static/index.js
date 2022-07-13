85e009b46afd0e38a61cc81b0fcad485f3ba123b
Illia <chippaxyucac@gmail.com>
2022-07-13 20:08:06 +0200

    Vue components

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,26 +1,18 @@
 
+Vue.component('fuck',
+    {
+        props: ['item'],
+        template: '<h2>{{ item.text }}</h2>'
+    })
 
 
-var app = new Vue({
+let app = new Vue({
     el: '#app-2',
     data: {
-        text: 'Вы загрузили эту страницу: ' + new Date().toLocaleString(),
-        seen : true,
-        address: [
-            {text: 'HTML/CSS' },
-            {text: 'JavaScript' },
-            {text : 'Vue'}
+        list : [
+            {id: 0, text: 'Fuck1'},
+            {id: 1, text: 'Fuck2'},
+            {id: 2, text: 'Fuck3'}
         ]
-    },
-
-    methods : {
-      reverseMessage : function (){
-          this.text = this.text.split('').reverse().join('')
-      }
     }
-
-
 })
-
-
-// app.seen = false
